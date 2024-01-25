e8f1a0c85fb9448e06bb657fb39a7d84532c89c6
wang <wanglaga17@gmail.com>
2024-01-26 05:10:08 +0700

    testing 10

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,18 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzm3x4iv5LzAyZhob3Z0lcfNHX4H1U3gJZ7njuoHbtlLPte3qleQpkV-KKZrbqWVJzr/exec'
+const form = document.forms['Pelanggaran SMP DW'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
 
-
-const datepickerEl = document.getElementById('tanggal');
-new Datepicker(datepickerEl, {
-    // options
-}); 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    btnLoading.classList.toggle('hidden');
+    btnKirim.classList.toggle('hidden');
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            btnLoading.classList.toggle('hidden');
+            btnKirim.classList.toggle('hidden');
+            location.replace('sukses.html');
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
